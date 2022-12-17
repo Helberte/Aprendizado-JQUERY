aced87e09662222cc50442e8022df194f7a47a78
helberte costa arruda <helberteads@gmail.com>
2022-12-16 22:26:06 -0400

    seletores

---
+++ seletores/js/script.js
@@ -0,0 +1,21 @@
+// nome de tag
+console.log($('h1').css('color', 'red'));
+
+// pelo id
+$('#logo').html('Negócios');
+
+// selecionando pela classe
+
+// $('.paragrafo').animate({
+//   fontSize: '15px',
+//   opacity: 0.4
+// }, 2000);
+
+//seletor composto, utilizando o nome ta tag e também o nome da class
+
+// $('p.paragrafo').animate({
+//   fontSize: '25px',
+//   opacity: 0.5
+// },2000);
+
+$('.header-menu ul, li').css('border', '1px yellow solid');

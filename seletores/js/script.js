f47748365274fc5c8896dff3d7fbeec63ed72fc2
helberte costa arruda <helberteads@gmail.com>
2022-12-18 17:59:26 -0400

    trabalhando com seletores

--- seletores/js/script.js
+++ seletores/js/script.js
@@ -1,21 +1,23 @@
 // nome de tag
 console.log($('h1').css('color', 'red'));
 
-// pelo id
-$('#logo').html('Negócios');
-
-// selecionando pela classe
-
-// $('.paragrafo').animate({
-//   fontSize: '15px',
-//   opacity: 0.4
-// }, 2000);
-
-//seletor composto, utilizando o nome ta tag e também o nome da class
-
-// $('p.paragrafo').animate({
-//   fontSize: '25px',
-//   opacity: 0.5
-// },2000);
-
 $('.header-menu ul, li').css('border', '1px yellow solid');
+
+$(document).ready(function(){
+  document.getElementById('h1-titulo').classList.add('active');
+});
+
+$('main>li').css('color', 'yellow');
+
+// selecionando a tag p mais proxima do .paragrafo
+
+$('.paragrafo+p').css({
+  'background-color': 'blue',
+  'font-size': '17px '
+});
+
+// selecionando todas que estão próximas ao .paragrafo
+$('.paragrafo~p').css({
+  'background-color': 'red',
+  'font-size': '17px '
+});
---
+++ seletores/js/seletores_basicos.js
@@ -0,0 +1,17 @@
+
+// pelo id
+$('#logo').html('Negócios');
+
+// selecionando pela classe
+
+// $('.paragrafo').animate({
+//   fontSize: '15px',
+//   opacity: 0.4
+// }, 2000);
+
+//seletor composto, utilizando o nome ta tag e também o nome da class
+
+// $('p.paragrafo').animate({
+//   fontSize: '25px',
+//   opacity: 0.5
+// },2000);
